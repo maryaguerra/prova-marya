981745555535b703761f8be0d5b0d5c413b963f5
Marya <169068359+maryaguerra@users.noreply.github.com>
2024-07-04 10:56:58 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("digite seu nome");
+    String nome = sc.next();
+     System.out.println("digite seu sobrenome");
+      String sobrenome = sc.next();
+      System.out.println("digite sua idade");
+       String idade = sc.next();
+       System.out.println("digite sua altura");
+       String altura = sc.next();
+       System.out.println(" digite seu peso");
+      String peso = sc.next();
+     System.out.println("Digite seu sexo");
+     System.out.println("1 - Feminino");
+    System.out.println("2 - Masculino");
+    int feminino = 1;
+    int masculino = 2;
+    
+  }
+}
